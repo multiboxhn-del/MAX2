4056edb67093ed80b05bc0ac88cc982bb715e4d9
SAMURAI <insamuray@gmail.com>
2025-10-26 10:33:38 -0600

    nuevos papa

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
-import Player from "./components/Player";
+import OnlineRadioPlayer from "./components/Player";
 
 export default function App() {
-  return <Player />;
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4">
+      <OnlineRadioPlayer
+        streamUrl="https://app.sonicpanelradio.com/8246/stream"
+        stationName="MAX RADIO"
+        coverFallback="https://eslamax.com/wp-content/uploads/2025/03/cropped-3.png"
+        // statusUrl="https://TU_STATUS_JSON" // opcional si tienes Icecast/Shoutcast JSON
+        themeColor="#8A3D9C"
+      />
+    </div>
+  );
 }
