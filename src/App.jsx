9ebca3c7244c45eebf48da3a97043979d71aa9e6
SAMURAI <insamuray@gmail.com>
2025-10-26 09:49:22 -0600

    Añadir reproductor a la App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,12 @@
 import Player from "./components/Player";
+import './App.css'; // Te recomiendo importar también un CSS para App
 
-export default function App(){ return <div>MAX Radio</div>; }
+export default function App() {
+  return (
+    // Usa un "fragment" (<>...</>) para devolver múltiples elementos
+    <>
+      <h1>MAX Radio</h1> {/* Es mejor usar una etiqueta <h1> para el título */}
+      <Player /> {/* ¡Aquí usamos el componente Player! */}
+    </>
+  );
+}
