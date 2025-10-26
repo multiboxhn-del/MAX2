630fff59d1077023c30814238857d5fd15ed69ca
SAMURAI <insamuray@gmail.com>
2025-10-26 10:00:42 -0600

    Mejoras de la interfaz

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-export default defineConfig({ plugins: [react()] });   // sin root, sin base rara
+export default defineConfig({ plugins: [react()] });
